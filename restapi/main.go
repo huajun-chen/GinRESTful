eff409bce86e44e1b3451c193e7b89078ba647ca
kkxl <chj655700@163.com>
2022-12-07 02:58:19 +0800

    优雅关机

--- restapi/main.go
+++ restapi/main.go
@@ -1,35 +1,26 @@
 package main
 
 import (
-	"GinRESTful/restapi/global"
 	"GinRESTful/restapi/initialize"
-	"fmt"
 	"github.com/fatih/color"
-	"go.uber.org/zap"
 )
 
 func main() {
 	// 1.初始化YAML配置
 	initialize.InitConfig()
-	// 2.初始化router
-	Router := initialize.InitRouter()
-	// 3.初始化日志信息
+	// 2.初始化日志信息
 	initialize.InitLogger()
-	// 4.初始化翻译
+	// 3.初始化翻译
 	if err := initialize.InitTrans("zh"); err != nil {
 		panic(err)
 	}
-	// 5.初始化MySQL
+	// 4.初始化MySQL
 	initialize.InitMysqlDB()
-	// 6.初始化Redis
+	// 5.初始化Redis
 	initialize.InitRedis()
-	// 7.初始化admin账户
+	// 6.初始化admin账户
 	initialize.InitAdminAccount()
-
 	color.Cyan("Gin服务开始了...")
-	// 启动Gin，并配置端口
-	err := Router.Run(fmt.Sprintf(":%d", global.Settings.Port))
-	if err != nil {
-		zap.L().Info("This is main.go", zap.String("error", "main启动错误..."))
-	}
+	// 7.启动Gin服务（优雅关闭）
+	initialize.RunServer()
 }
